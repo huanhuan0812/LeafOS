bdb3e3e62e53ad70cb7852b2d7115c1cf0bb8ed2
huanhuan0812 <qdhuanhuan08@outlook.com>
2026-04-25 20:05:54 +0800

    change to rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#![no_std]
+
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+    uefi::println!("Hello, world!");
+    Status::SUCCESS
+}
